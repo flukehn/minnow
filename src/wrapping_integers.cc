03e5c89f020a6e602b305ee76636610813582d93
flukehn <flukehn@gmail.com>
2023-06-24 13:29:47 +0000

    wrap32 pass

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,33 @@
 #include "wrapping_integers.hh"
+#include <algorithm>
+#include <cstdint>
+#include <type_traits>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
   // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { static_cast<uint32_t>(n) + zero_point.raw_value_ };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t tail = wrap(checkpoint, zero_point).raw_value_;
+  uint32_t raw = raw_value_;
+  uint64_t ret = checkpoint - tail + raw;
+  uint64_t ret2 = ret + (1ULL<<32);
+  if (tail > checkpoint + raw) {
+    return ret2;
+  }
+  if (tail < raw) {
+    if (ret < (1ULL<<32)) {
+      return ret;
+    }
+    ret2 -= 1ULL<<33;
+    std::swap(ret, ret2);
+  }
+  return checkpoint - ret < ret2 - checkpoint ?
+     ret : ret2;
 }
