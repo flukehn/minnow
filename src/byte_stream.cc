8e583aeeee89372b57427f9f81cd7504ef9c0d57
flukehn <flukehn@gmail.com>
2023-06-24 02:19:21 +0000

    check0 pass

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,77 +1,101 @@
+#include <cassert>
+#include <cstdint>
+#include <iostream>
 #include <stdexcept>
 
 #include "byte_stream.hh"
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), 
+  lpos(0), rpos(0), buf(), is_closed_(false), error_(false) {
+  buf.resize(capacity_);
+}
 
 void Writer::push( string data )
 {
   // Your code here.
-  (void)data;
+  //assert(data.size() + rpos - lpos <= capacity_);
+  uint64_t len = available_capacity();
+  if (len > data.size()) {
+    len = data.size();
+  }
+  for(uint64_t i=0;i<len;++i){
+    buf[(rpos++)%capacity_]=data[i];
+  }
 }
 
 void Writer::close()
 {
   // Your code here.
+  is_closed_ = true;
 }
 
 void Writer::set_error()
 {
   // Your code here.
+  error_ = true;
 }
 
 bool Writer::is_closed() const
 {
   // Your code here.
-  return {};
+  return is_closed_;
 }
 
 uint64_t Writer::available_capacity() const
 {
   // Your code here.
-  return {};
+  return capacity_ - (rpos - lpos);
 }
 
 uint64_t Writer::bytes_pushed() const
 {
   // Your code here.
-  return {};
+  return rpos;
 }
 
 string_view Reader::peek() const
 {
   // Your code here.
-  return {};
+  if (lpos == rpos) {
+    return "";
+  }
+  uint64_t r = rpos % capacity_;
+  uint64_t l = lpos % capacity_;
+  if (l < r) {
+    return {buf.data()+l, buf.data()+r};
+  } else {
+    return {buf.data() + l, buf.data() + capacity_};
+  }
 }
 
 bool Reader::is_finished() const
 {
   // Your code here.
-  return {};
+  return !error_ && is_closed_ && rpos == lpos;
 }
 
 bool Reader::has_error() const
 {
   // Your code here.
-  return {};
+  return error_;
 }
 
 void Reader::pop( uint64_t len )
 {
   // Your code here.
-  (void)len;
+  lpos += len;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
   // Your code here.
-  return {};
+  return rpos - lpos;
 }
 
 uint64_t Reader::bytes_popped() const
 {
   // Your code here.
-  return {};
+  return lpos;
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <queue>
 #include <stdexcept>
 #include <string>
@@ -13,7 +14,11 @@ class ByteStream
 protected:
   uint64_t capacity_;
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
-
+  uint64_t lpos;
+  uint64_t rpos;
+  std::string buf;
+  bool is_closed_;
+  bool error_;
 public:
   explicit ByteStream( uint64_t capacity );
 
